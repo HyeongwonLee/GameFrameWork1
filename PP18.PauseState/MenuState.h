b0f1a3c1ea56ad73e297bdc549ad8edea8d5dc53
형원 이 <20171249@vision.hoseo.edu>
2018-12-05 00:44:07 +0900

    PP18.PauseState

--- PP18.PauseState/MenuState.h
+++ PP18.PauseState/MenuState.h
@@ -1,1 +1,30 @@
 #pragma once
+#include"GameState.h"
+class MenuState : public GameState
+{
+public:
+	virtual void update();
+	virtual void render();
+	virtual bool onEnter();
+	virtual bool onExit();
+	virtual std::string getStateID() const { return s_menuID; }
+
+	static MenuState* Instance()
+	{
+		if (s_pInstance == 0)
+		{
+			s_pInstance = new MenuState();
+			return s_pInstance;
+		}
+		return s_pInstance;
+	}
+	MenuState() {}
+private:
+	static void s_menuToPlay();
+	static void s_exitFromMenu();
+
+	~MenuState() {}
+	static const std::string s_menuID;
+	static MenuState* s_pInstance;
+};
+typedef MenuState TheMenuState;
